c291fbf4000e4cba55195f35f0ea82a829fe09ec
AhmetOsmn <Ahmet.osmn@outlook.com>
2022-05-20 20:00:40 +0300

    writerlayout eklendi

--- BlogProject/Controllers/WriterController.cs
+++ BlogProject/Controllers/WriterController.cs
@@ -19,5 +19,23 @@ namespace BlogProject.Controllers
         {
             return View();
         }
+
+        [AllowAnonymous]
+        public IActionResult Test()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        public PartialViewResult WriterNavbarPartial()
+        {
+            return PartialView();   
+        }
+
+        [AllowAnonymous]
+        public PartialViewResult WriterFooterPartial()
+        {
+            return PartialView();   
+        }
     }
 }
