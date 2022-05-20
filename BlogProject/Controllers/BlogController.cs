705ac816b33cc8caeec1750387bc5756f87e5118
AhmetOsmn <Ahmet.osmn@outlook.com>
2022-05-20 21:22:01 +0300

    Blog ekleme islemi eklendi

--- BlogProject/Controllers/BlogController.cs
+++ BlogProject/Controllers/BlogController.cs
@@ -1,9 +1,14 @@
 ﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationsRules;
 using DataAccessLayer.Concrete.EntityFramework;
 using EntityLayer.Concrete;
+using FluentValidation.Results;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace BlogProject.Controllers
 {
@@ -30,6 +35,44 @@ namespace BlogProject.Controllers
             return View(blogs);
         }
 
+        [HttpGet]
+        public IActionResult BlogAdd()
+        {
+            CategoryManager categoryManager = new(new EfCategoryRepository());
+            List<SelectListItem> categories = (from x in categoryManager.GetAll()
+                                               select new SelectListItem
+                                               {
+                                                   Text = x.Name,
+                                                   Value = x.Id.ToString()
+                                               }).ToList();
+            ViewBag.categoryValues = categories;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult BlogAdd(Blog blog)
+        {
+            BlogValidator validationRules = new();
+
+            ValidationResult results = validationRules.Validate(blog);
+
+            if (results.IsValid)
+            {
+                blog.Status = true;
+                blog.CreateDate = DateTime.Parse(DateTime.Now.ToShortDateString());
+                blog.WriterId = 1;
+                blogManager.Add(blog);
+                return RedirectToAction("BlogListByWriter", "Blog");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
+        }
 
     }
 }
---
+++ BusinessLayer/ValidationsRules/BlogValidator.cs
@@ -0,0 +1,22 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+
+namespace BusinessLayer.ValidationsRules
+{
+    public class BlogValidator : AbstractValidator<Blog>
+    {
+        public BlogValidator()
+        {
+            RuleFor(x => x.Title).NotEmpty().WithMessage("Blog başlık kısmı boş geçilemez.");
+            RuleFor(x => x.Title).MaximumLength(50).WithMessage("En fazla 50 karakter giriniz.");
+            RuleFor(x => x.Title).MinimumLength(2).WithMessage("En az 2 karakter giriniz.");
+
+
+            RuleFor(x => x.Content).NotEmpty().WithMessage("Blog içerik kısmı boş geçilemez.");
+            RuleFor(x => x.Content).MaximumLength(350).WithMessage("En fazla 350 karakter giriniz.");
+            RuleFor(x => x.Content).MinimumLength(2).WithMessage("En az 2 karakter giriniz.");
+
+            RuleFor(x => x.Image).NotEmpty().WithMessage("Blog görseli kısmı boş geçilemez.");
+        }
+    }
+}
