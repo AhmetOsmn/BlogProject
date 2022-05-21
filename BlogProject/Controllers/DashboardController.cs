b9bd68d17d77475cdf05565d7096897c78ccb6df
AhmetOsmn <Ahmet.osmn@outlook.com>
2022-05-21 20:42:32 +0300

    Dashboard duzenlendi ve istatistikler eklendi

--- BlogProject/Controllers/DashboardController.cs
+++ BlogProject/Controllers/DashboardController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using DataAccessLayer.Concrete;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace BlogProject.Controllers
 {
@@ -8,6 +10,10 @@ namespace BlogProject.Controllers
         [AllowAnonymous]
         public IActionResult Index()
         {
+            Context contex = new();
+            ViewBag.v1 = contex.Blogs.Count().ToString();
+            ViewBag.v2 = contex.Blogs.Where(x => x.WriterId == 1).Count().ToString();
+            ViewBag.v3 = contex.Categories.Count().ToString();
             return View();
         }
     }
