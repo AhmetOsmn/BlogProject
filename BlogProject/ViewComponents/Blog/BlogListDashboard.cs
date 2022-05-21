c7ef9e04d28817ef9c090e89662464e3be34d2b7
AhmetOsmn <Ahmet.osmn@outlook.com>
2022-05-21 19:04:32 +0300

    dashboard duzenlemeleri yapildi

---
+++ BlogProject/ViewComponents/Blog/BlogListDashboard.cs
@@ -0,0 +1,17 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogProject.ViewComponents.Blog
+{
+    public class BlogListDashboard : ViewComponent
+    {
+        BlogManager blogManager = new(new EfBlogRepository());
+
+        public IViewComponentResult Invoke()
+        {
+            var values = blogManager.GetBlogListWithCategory();
+            return View(values);
+        }
+    }
+}
---
+++ BlogProject/ViewComponents/Category/CategoryListDashboard.cs
@@ -0,0 +1,17 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogProject.ViewComponents.Category
+{
+    public class CategoryListDashboard : ViewComponent
+    {
+        CategoryManager categoryManager = new(new EfCategoryRepository());
+
+        public IViewComponentResult Invoke()
+        {
+            var values = categoryManager.GetAll();
+            return View(values);
+        }
+    }
+}
