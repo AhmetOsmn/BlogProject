68b65b02ef6182d8607d3454b8d1782cd6473f87
AhmetOsmn <Ahmet.osmn@outlook.com>
2022-05-22 15:54:47 +0300

    Bildirimlerin listelenmesi tamamlandi

--- BlogProject/Controllers/NotificationController.cs
+++ BlogProject/Controllers/NotificationController.cs
@@ -1,12 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete.EntityFramework;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BlogProject.Controllers
 {
     public class NotificationController : Controller
     {
+        NotificationManager notificationManager = new(new EfNotificationRepository());
         public IActionResult Index()
         {
             return View();
+        }
+
+        [AllowAnonymous]
+        public IActionResult GetAllNotification()
+        {
+            var values = notificationManager.GetAll();
+            return View(values);
         }
     }
 }
