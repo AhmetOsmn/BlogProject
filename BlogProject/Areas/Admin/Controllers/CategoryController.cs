5d2116a76fa4604bd97d9ce8f17ab5e060b065f0
AhmetOsmn <Ahmet.osmn@outlook.com>
2022-05-23 10:38:03 +0300

    admin tarafından kategori ekleme islemi ve sayfalama islemi eklendi

--- BlogProject/Areas/Admin/Controllers/CategoryController.cs
+++ BlogProject/Areas/Admin/Controllers/CategoryController.cs
@@ -1,13 +1,54 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationsRules;
+using DataAccessLayer.Concrete.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
+using Microsoft.AspNetCore.Mvc;
+using X.PagedList;
 
 namespace BlogProject.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class CategoryController : Controller
     {
-        public IActionResult Index()
+        CategoryManager categoryManager = new(new EfCategoryRepository());
+        public IActionResult Index(int page= 1)
+        {
+            var values = categoryManager.GetAll().ToPagedList(page,10);
+            return View(values);
+        }
+
+        #region AddCategory
+
+        [HttpGet]
+        public IActionResult AddCategory()
         {
             return View();
         }
+
+        [HttpPost]
+        public IActionResult AddCategory(Category category)
+        {
+            CategoryValidator validator = new();
+            ValidationResult results = validator.Validate(category);
+
+            if (results.IsValid)
+            {
+
+                category.Status = true;
+                categoryManager.Add(category);
+                return RedirectToAction("Index", "Category");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
+        }
+
+        #endregion
     }
 }
---
+++ BusinessLayer/ValidationsRules/CategoryValidator.cs
@@ -0,0 +1,23 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationsRules
+{
+    public class CategoryValidator : AbstractValidator<Category>
+    {
+        public CategoryValidator()
+        {
+            RuleFor( x=> x.Name).NotEmpty().WithMessage("Kategori adı boş olamaz.");
+            RuleFor( x=> x.Name).MaximumLength(50).WithMessage("En fazla 50 karakter giriniz.");
+            RuleFor( x=> x.Name).MinimumLength(2).WithMessage("En az 2 karakter giriniz.");
+            
+            RuleFor( x=> x.Description).NotEmpty().WithMessage("Açıklama kısmı boş olamaz.");
+
+        }
+    }
+}
