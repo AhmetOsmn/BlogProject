be855c38718129a0fd587ca601648307ef2bca8b
AhmetOsmn <Ahmet.osmn@outlook.com>
2022-05-23 12:38:46 +0300

    widget ve istatistik sayfalari duzenlenmeye baslandi

---
+++ BlogProject/Areas/Admin/ViewComponents/Statistic/StatisticRow1.cs
@@ -0,0 +1,20 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.Concrete.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace BlogProject.Areas.Admin.ViewComponents.Statistic
+{
+    public class StatisticRow1 : ViewComponent
+    {
+        Context context = new();  
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.BlogCount = context.Blogs.Count();
+            ViewBag.ContactCount = context.Contacts.Count();    
+            ViewBag.CommentCount = context.Comments.Count();
+            return View();
+        }
+    }
+}
---
+++ BlogProject/Areas/Admin/ViewComponents/Statistic/StatisticRow2.cs
@@ -0,0 +1,18 @@
+﻿using DataAccessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace BlogProject.Areas.Admin.ViewComponents.Statistic
+{
+    public class StatisticRow2 : ViewComponent
+    {
+        Context context = new();
+        public IViewComponentResult Invoke()
+        {
+            var latestBlog = context.Blogs.OrderByDescending(x => x.Id).Take(1).FirstOrDefault();
+            ViewBag.LatestBlogTitle = latestBlog.Title;
+            ViewBag.LatestBlogDate = latestBlog.CreateDate.ToShortDateString();
+            return View();
+        }
+    }
+}
