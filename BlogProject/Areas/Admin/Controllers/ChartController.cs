0316a2a4e81b41847cec80881a6007870f14a509
AhmetOsmn <Ahmet.osmn@outlook.com>
2022-05-24 13:09:45 +0300

    google chart örneği eklendi

---
+++ BlogProject/Areas/Admin/Controllers/ChartController.cs
@@ -0,0 +1,26 @@
+﻿using BlogProject.Areas.Admin.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace BlogProject.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ChartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CategoryChart()
+        {
+            List<GetCategoryViewModel> categoryViewModels = new List<GetCategoryViewModel>();
+
+            categoryViewModels.Add(new GetCategoryViewModel { categoryname = "Yazılım", blogcount = 10 });
+            categoryViewModels.Add(new GetCategoryViewModel { categoryname = "Spor", blogcount = 20 });
+            categoryViewModels.Add(new GetCategoryViewModel { categoryname = "Oyun", blogcount = 30 });
+
+            return Json(new { jsonlist = categoryViewModels });
+        }
+    }
+}
---
+++ BlogProject/Areas/Admin/Models/GetCategoryViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace BlogProject.Areas.Admin.Models
+{
+    public class GetCategoryViewModel
+    {
+        public int blogcount { get; set; }
+        public string categoryname { get; set; }
+    }
+}
